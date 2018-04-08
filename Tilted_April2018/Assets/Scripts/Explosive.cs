336f3b5528c4d4e241b4a18ad52efff576104fab
Joel Lee <lee.joel395@gmail.com>
2018-04-08 15:26:00 -0700

    PlaymakerAdded AI Stuff

---
+++ Tilted_April2018/Assets/Scripts/Explosive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour {
+    //don't actyally touch this
+    [SerializeField]
+    private IDamageable _damageableObj;
+
+    public void Explode() {
+        if(_damageableObj != null) {
+            Debug.Log(_damageableObj);
+            _damageableObj.TakeDamage(999f);
+        }
+    }
+
+    void OnTriggerEnter(Collider c) {
+        if (c.gameObject.tag == "Player") {
+            _damageableObj = c.gameObject.GetComponent<IDamageable>();
+        }
+    }
+}
---
+++ Tilted_April2018/Assets/Scripts/JalepenoEntity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class JalepenoEntity : MonoBehaviour {
+    [SerializeField]
+    protected float _speed = 3.0f;
+    [SerializeField]
+    protected float _explosionDelay = 2.0f;
+
+    public NavMeshAgent NMAgent;
+    public Explosive explosion;
+    protected GameObject _playerObject;
+    private Vector3 _dir;
+	// Use this for initialization
+	void Start () {
+        _playerObject = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            StartCoroutine(Explode());
+        }
+	}
+
+    public void ChasePlayer() {
+        _dir = (transform.position - _playerObject.transform.position).normalized;
+        NMAgent.Move(_dir * _speed * Time.deltaTime);
+    }
+
+    public IEnumerator Explode() {
+        yield return new WaitForSeconds(_explosionDelay);
+        explosion.gameObject.SetActive(true);
+    }
+
+}
